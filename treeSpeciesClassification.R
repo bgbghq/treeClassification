b9d666b6602145f66aa7600b8cca75dbaa1eae21
Andy Whelan <94721012+bgbghq@users.noreply.github.com>
2023-06-21 11:26:15 -0400

    Creates a dataset with reflectance values for each spectra at each pixel for each crown and species classifications for each crown, but the model prediction pretty well suck.

--- treeSpeciesClassification.R
+++ treeSpeciesClassification.R
@@ -10,7 +10,7 @@
 #---> setup
 # Libraries
 packages = c("httr", "jsonlite","ggplot2","neonUtilities","neonOS","devtools","sf","terra","lidR","data.table",
-             "hsdar")
+             "stringr","Boruta","randomForest")
 for(package in packages) {
   if(suppressMessages(!require(package,character.only = T))){
     install.packages(package)
@@ -160,9 +160,9 @@ c2 = c1[c1$treeID %in% t2$treeID,] # Subset again to crowns with trees with meas
 c2 = c2[order(t2$treeID),]
 c2$taxonID = t2$taxonID # add taxonID
 st_write(c2, "crowns.shp")
-c2 = st_read("crowns.shp")
+c2 = st_read("../firstTry/crowns.shp")
 c3 = vect(c2)
-
+crowns = c3
 
 # ----------------------- Hyperspectral data -----------------------------------------------------
 # mask hyperspectral data
@@ -177,7 +177,11 @@ myNoDataValue = as.numeric(refInfo$Data_Ignore_Value)
 
 #---> Make a raster stack (pile) out of all the spectral bands
 band2Rast = function(file, band, noDataValue, extent, CRS){
-  out = h5read(file, "/JERC/Reflectance/Reflectance_Data", index=list(band,NULL,NULL))
+  if(is.character(file)) {
+    out = h5read(file, "/JERC/Reflectance/Reflectance_Data", index=list(band,NULL,NULL))
+  }else{
+    out = file
+  }
   out = aperm(out, c(3,2,1)) # reshape the array to ('rows','columns','bands') or something like that.
   out[out == noDataValue] = NA
   outr = rast(out,crs=CRS)
@@ -213,3 +217,194 @@ m1 = tiles[[1]]
 for(i in 2:length(tiles)) {
   m1 = merge(m1, tiles[[i]])
 }
+
+# make a list of the band numbers
+bList = seq(1, 426, 1)
+tileLons = as.numeric(unlist(lapply(hypList, function(x) substr(x, 19, 24))))
+tileLats = as.numeric(unlist(lapply(hypList, function(x) substr(x, 26,32))))
+tileCoords = data.frame(lon = tileLons, lat = tileLats)
+
+
+#----------------------------------- Try to extract spectra without using so much memory -----------------
+# function to get the spectra in the area of each crown
+# "crowns" is an sf file with crown polygons. origin: lower left
+# origin of NEON spectral data is upper left
+
+
+bigMatrix = list()
+for(i in 1:nrow(crowns)) { # for each crown
+  
+  # Figure out which hyperspectral tile to load and load it as a raster
+  extTmp = ext(crowns[i,]) # bounding box of the crown
+  
+  lonTmp = floor(extTmp[1]/1000)*1000
+  latTmp = floor(extTmp[3]/1000)*1000
+  
+  paths = c(hypList[grep(paste0(lonTmp,"_",latTmp), hypList)], lonTmp, latTmp)
+  
+  tTmp = H5Fopen(paste0(hypPath,paths[1]))
+  t2Tmp = tTmp&'JERC/Reflectance/Reflectance_Data'
+  
+  tmpMatrix = list()
+  for(j in 1:426) { # for each wavelength
+    thing = t2Tmp[j,,] # [bands,columns,rows], origin: upper left
+    thing[thing == -9999] = NA
+    thing = aperm(thing, c(2,1))
+    
+    # create a matrix of pixel values
+    thing2 = rast(thing, crs=myCRS, extent=c(lonTmp,lonTmp+1000,latTmp,latTmp+1000))
+    thing3 = crop(thing2, crowns[i])
+    thing3 = mask(thing3, crowns[i], touches=F)
+    thing4 = as.vector(thing3)
+    
+    # Change the matrix into a vector
+    tmpMatrix[[length(tmpMatrix)+1]] = as.numeric(na.omit(thing4))
+  }
+  
+  # Close the dataset
+  h5closeAll()
+  
+  # Put the vectors into a matrix as columns and add a column that specifies the tree species
+  tmpMatrix = do.call(cbind, tmpMatrix)
+  tmpDf = data.frame(tmpMatrix, taxonID=crowns[i]$taxonID)
+  
+  # Put the matrix into the bigMatrix list
+  bigMatrix[[length(bigMatrix)+1]] = tmpDf
+}
+# rbind the matrices to make a longer matrix
+outFile = do.call(rbind, bigMatrix)
+
+write.csv(outFile, "SpecModelData.csv")
+outFile = read.csv("SpecModelData.csv")
+head(outFile)
+outFile = outFile[,2:428]
+dim(outFile)
+boruta = Boruta(x=outFile[,1:426], y=as.factor(outFile[,427]), doTrace=0, pValue=0.05)
+
+finalvars = getSelectedAttributes(boruta, withTentative = F)
+
+(modelRF_class = randomForest(x=outFile[ , finalvars], y=as.factor(outFile$taxonID), # This get's the response 
+                           # variable to be BA per 20m pixel
+                           importance = TRUE))
+    
+  crownChunks = list()
+    extTmp = ext(crowns[i,]) # bounding box of the crown
+    
+    lonTmp = floor(extTmp[1]/1000)*1000
+    latTmp = floor(extTmp[3]/1000)*1000
+    lonMin = floor(extTmp[1,]) # round down
+    lonMax = ceiling(extTmp[2]) # round up
+    latMin = floor(extTmp[3,]) # round down
+    latMax = ceiling(extTmp[4]) # round up
+    
+    # Get paths to all tiles the crown is in (up to four)
+    paths = list()
+    paths[[length(paths)+1]] = c(hypList[grep(paste0(lonTmp,"_",latTmp), hypList)], lonTmp, latTmp)
+    if(latMax-latTmp > 1000) paths[[length(paths)+1]] = 
+      c(hypList[grep(paste0(lonTmp,"_",latTmp+1000),hypList)], lonTmp, latTmp+1000)
+    if(lonMax-lonTmp > 1000) paths[[length(paths)+1]] = 
+      c(hypList[grep(paste0(lonTmp+1000,"_",latTmp),hypList)], lonTmp+1000, latTmp)
+    if(lonMax-lonTmp > 1000 & latMax-latTmp > 1000) {
+      paths[[length(paths)+1]] = c(hypList[grep(paste0(lonTmp,"_",latTmp+1000),hypList)], lonTmp, latTmp+1000)
+      paths[[length(paths)+1]] = c(hypList[grep(paste0(lonTmp+1000,"_",latTmp),hypList)], lonTmp+1000, latTmp)
+      paths[[length(paths)+1]] = c(hypList[grep(paste0(lonTmp+1000,"_",latTmp+1000),hypList)], lonTmp+1000, latTmp+1000)
+    }
+    
+    for(j in 1:length(paths)) {
+      
+      # find the relative coordinates of the crown to subset
+      xmin = round(extTmp[1])-as.numeric(paths[[j]][2]) # x extents in unprojected hyperspectral array
+      if(xmin < 0) xmin=1
+      xmax = round(extTmp[2])-as.numeric(paths[[j]][2])
+      if(xmax > 1000) xmax=1000
+      ymin = round(extTmp[3])-as.numeric(paths[[j]][3]) # y extents in unprojected hyperspectral array
+      if(ymin < 0) ymin=1
+      ymax = round(extTmp[4])-as.numeric(paths[[j]][3])
+      if(ymax > 1000) ymax=1000
+      
+      # Open the tile the crown starts in and mask it
+      tTmp = H5Fopen(paste0(hypPath,paths[[j]][1]))
+      t2Tmp = tTmp&'JERC/Reflectance/Reflectance_Data'
+      thing = t2Tmp[,xmin:xmax,ymin:ymax] # [bands,columns,rows], origin: upper left
+      thing[thing == -9999] = NA
+      thing = aperm(thing, c(3,2,1))
+      thing2 = rast(thing, crs=myCRS, extent=c(lonTmp,lonTmp+1000,latTmp,latTmp+1000))
+      ext(thing2) = c(xmin,xmax,ymin,ymax)
+      res(thing2) = c(1,1)
+      crownChunks[[length(crownChunks)+1]] = thing2
+    }
+    crownChunk = do.call(merge, crownChunks)
+    ext(crownChunk) = c(lonMin,lonMax,latMin,latMax)
+  }
+  
+}
+    
+barf = 
+
+
+    # Identify the path(s) to the hyperspectral data tile(s).
+    if(extTmp[4]-latTmp > 1000) { # if the crown strays latitudinally into another tile
+      paths = paste0(paths, ",",hypList[grep(paste0(lonTmp+1000,"_",latTmp), hypList)])
+      side = 1
+    }
+    if(extTmp[2]-lonTmp > 1000) { # If the crown strays longitudinally into another tile
+      paths = paste0(paths,",", hypList[grep(paste0(lonTmp,"_",latTmp+1000), hypList)])
+      side = 2
+    }
+    if(extTmp[2]-lonTmp > 1000 & extTmp[4]-latTmp > 1000) { # If the crown strays into 3 other tiles
+      paths = paste0(paths,",", hypList[grep(paste0(lonTmp+1000,"_",latTmp+1000), hypList)])
+      side = 3
+    }
+    
+    # if there are more than one path, split them
+    paths = strsplit(paths, ",")
+    
+    
+    
+    tmpSpectra = list()
+    for(j in 1:length(paths)) {
+      
+      # got to get the right part of each tile
+      if(i == 1){
+        tTmp2 = tTmp&'JERC/Reflectance/Reflectance_Data'[]
+      }
+      if(i == 2 & side == 1){
+        tTmp2 = tTmp&'JERC/Reflectance/Reflectance_Data'[]
+      }
+      if(i == 2 & side == 2)
+      bigTile[[length(bigTile)+1]] = tTmp2[]
+    }
+    
+  }
+  
+      # subset the data
+      t3 = t2[,xmin:xmax,ymin:ymax]
+      
+      # Get attributes (projection and crs)
+      a1 = tile&'/JERC/Reflectance/Metadata/Coordinate_System/'
+      tmpCRS = paste0("+init=epsg:", a1$`EPSG Code`)
+      
+      # make a spatial extent for the crown
+      utmXmin = lonTmp+xmin
+      utmXmax = lonTmp+xmax
+      utmYmin = latTmp+ymin
+      utmYmax = latTmp+ymax
+      utmExt = ext(utmXMin, utmXmax, utmYmin, utmYmax)
+      
+      # make it into a raster
+      spat = band2Rast(t3, bList, -9999, extent=utmExt,CRS=tmpCRS)
+      bandNames = paste("Band_",unlist(bList),sep="")
+      names(spat) = bandNames
+      tiles[length(tiles) + 1] = mask(spat, c3)
+      # close dataset 
+      h5closeAll()
+
+    }
+    
+    
+    tile1 = H5Fopen()
+    print(extTmp)
+  }
+}
+crownSpectra(crowns)
+crowns = read_sf("../firstTry/crowns.shp")
